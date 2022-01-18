463cbb25668b79a7cf1b380d2abfd0624aef2c1b
StarDust85z <roadtomars2030@gmail.com>
2022-01-18 14:25:48 +0300

    users and reactions added

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -1,6 +1,10 @@
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
+import { PostAuthor } from './PostAuthor'
+import { TimeAgo } from './TimeAgo'
+import { ReactionButtons } from './ReactionButtons'
+
 export const SinglePostPage = ({match}) => {
     const {postId} = match.params
 
@@ -20,7 +24,10 @@ export const SinglePostPage = ({match}) => {
         <section>
             <article className='post'>
                 <h2>{post.title}</h2>
+                <PostAuthor userId={post.user} />
+                <TimeAgo timestamp={post.date} />
                 <p className="post-content">{post.content}</p>
+                <ReactionButtons post={post} />
                 <Link to={`/editPost/${post.id}`} className="button">
                     Edit Post
                 </Link>
